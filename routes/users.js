ba327257fd63cea2812873db1ad55f4c724a5d24
dalf-g <muriithi.andrie@gmail.com>
2025-10-14 16:04:48 +0300

    oct 24th

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 // import the user schema
 const {user} = require("../models/models")
@@ -21,4 +22,79 @@ router.get("/",auth, async(req, res)=>{
     }
 })
 
+// view a single user
+router.get("/:id",auth,async(req, res)=>{
+    try{
+        const User = await user.findById(req.params.id);
+
+        // console.log("The req.param.id is:",req.params.id)
+
+        // chech whether there is a record returned
+        if(!User){
+            return res.sendStatus(400).json({message: "sorry, user not FOUND....!"})
+        }
+
+        // if the user is there, return the details of the user
+        res.json(User);
+    }
+    catch{
+        res.status(500).json({message:err.message})
+    }
+})
+
+// update a user based on id
+router.put("/:id",async(req, res)=>{
+    try{
+        // destructure the content you want to update from the sent request
+        const{name, email,password} = req.body;
+
+        // console.log("the name entered is:",name);
+        // console.log("The email entered is:",email);
+        // console.log("The password entered is:",password)
+
+        // check if user exists or not based on the id passed as a parameter.
+        const User = await user.findById(req.params.id)
+        if(!User){
+            res.status(404).json({message: "Ooops, User not found...Please checK the id"})
+        }
+        // Prepare the update data
+        let updateData = {name,email}
+
+        if(password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            updateData.password = hashedPassword;
+        }
+
+        // update the user with the new details
+        const updateUser = await user.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            {new : true}
+        )
+        res.json(updateUser)
+    }
+    catch(err){
+        res.status(400).json({message : err.message})
+    }
+})
+
+// below is the delete router
+router.delete("/:id",async(req,res)=>{
+    try{
+        // attempt to find a user by his/her id and delete them
+        const deleteUser = await user.findByIdAndDelete(req.params.id)
+
+        // if the id of the user does not exist, give a response back to the person
+        if(!deleteUser){
+            res.status(404).json({message: "Ooops... User not found"})
+        }
+        // if deletion has happened successfully, send a success message
+        res.json({message: "User successfully deleted"})
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+// below
 module.exports = router
