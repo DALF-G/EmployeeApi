3b31ffe220f95c5a5a0ab6f47d75030e6c6d2394
dalf-g <muriithi.andrie@gmail.com>
2025-10-22 16:05:29 +0300

    updated data

--- routes/employees.js
+++ routes/employees.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const {employee, department} = require('../models/models');
 
 const auth = require("../middleware/auth");
-const { route } = require('./employees');
 
 
 // register a new employee
